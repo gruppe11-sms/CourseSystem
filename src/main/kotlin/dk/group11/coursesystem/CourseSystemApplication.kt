fb72bd8a2e21e62ecab41660f7a45ffa1be2db8c
Hugzy <frede.madsen3@gmail.com>
2017-11-02 13:19:52 +0100

    SMS-82 big refactor v3, changed a class name

--- src/main/kotlin/dk/group11/coursesystem/CourseSystemApplication.kt
+++ src/main/kotlin/dk/group11/coursesystem/CourseSystemApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class RoleSystemApplication
+class CourseSystemApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(RoleSystemApplication::class.java, *args)
+    SpringApplication.run(CourseSystemApplication::class.java, *args)
 }
